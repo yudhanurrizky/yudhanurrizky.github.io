a24b0f923ac942686f424c900f079502c95c55e2
Yudha Nurrizky <89990254+yudhanurrizky@users.noreply.github.com>
2022-03-31 13:29:06 +0700

    added interactive show duck button

---
+++ javascript/index.js
@@ -0,0 +1,7 @@
+/** GIF BUTTON */
+
+function showGif () {
+    document.getElementById("gif").style.display = "block"; 
+};
+
+document.getElementById("gif-button").addEventListener("click", showGif);
